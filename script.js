e940f63d95c76cf2b4cb29fa78f45e630da05f3e
Matt Bay <MJBayUNL@yahoo.com>
2021-11-13 10:57:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click',function() {
     document.body.classList.toggle('dark-theme');
 
-    var className = document.body.className;
+    let className = document.body.className;
     if(className == "light-theme"){
         this.textContent = "Dark";
     }else{
